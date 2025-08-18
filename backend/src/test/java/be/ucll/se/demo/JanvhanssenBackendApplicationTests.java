7e5044c0ec0ba4d534d09f31cc105965483644ad
JanVHanssen <r0906558@ucll.be>
2025-08-18 14:51:10 +0200

    oazierizohfdnsv

--- backend/src/test/java/be/ucll/se/demo/JanvhanssenBackendApplicationTests.java
+++ backend/src/test/java/be/ucll/se/demo/JanvhanssenBackendApplicationTests.java
@@ -2,9 +2,14 @@ package be.ucll.se.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class JanvhanssenBackendApplicationTests {
+
+	@MockBean
+	private JavaMailSender mailSender; // fake bean voor testcontext
 
 	@Test
 	void contextLoads() {
